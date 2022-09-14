68272521bf0ba1013120b20b77157ebbfa272bde
Artemvogor <90313333+Artemvogor@users.noreply.github.com>
2022-09-14 21:56:43 +0300

    Add files via upload

---
+++ Visual.cpp
@@ -0,0 +1,28 @@
+#include "Visual.h"
+#include <Windows.h>
+
+using namespace System;
+using namespace System::Windows::Forms;
+using namespace vebclr;
+
+
+[STAThread]
+//void main(array<String^>^ arg) {
+void main() {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    vebclr::Visual^ form = gcnew Visual();
+    form->tr = new Veb(32);
+    Application::Run(form);
+}
+
+
+
+//
+//
+//int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+//    Application::EnableVisualStyles();
+//    Application::SetCompatibleTextRenderingDefault(false);
+//    Application::Run(gcnew Visual);
+//    return 0;
+//}
